4282616573a05e2c20752da06ca332a23af2f312
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-09 21:50:45 -0400

    working on stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,11 +5,14 @@
 function getInfo(username){
 axios.get(`https://api.github.com/users/${username}`)
   .then(res=>{
+    // console.log(res)
+    
     return cards.appendChild(cardCreator(res))
     
   })
   .catch()
 }
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -22,21 +25,9 @@ axios.get(`https://api.github.com/users/${username}`)
 */
 let cards = document.querySelector('.cards')
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
 const followersArray = [];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-*/function cardCreator(obj){
+function cardCreator(obj){
   let cardParent = document.createElement('div');
   let cardImg = document.createElement('img');
   let info = document.createElement('div');
@@ -50,7 +41,6 @@ const followersArray = [];
   let pageUrl = document.createElement('a');
 
   // Add structure
-  // profile.appendChild(pageUrl)
   info.appendChild(name)
   info.appendChild(username)
   info.appendChild(geo)
@@ -77,11 +67,13 @@ const followersArray = [];
   followers.textContent = `Followers: ${obj.data.followers}`
   following.textContent = `Following: ${obj.data.following}`
   bio.textContent = obj.data.bio
+
+  profile.appendChild(pageUrl)
+
   cardParent.classList.add('card')
   info.classList.add('card-info')
   name.classList.add('name')
   username.classList.add('username')
-  profile.appendChild(pageUrl)
 
 
 
